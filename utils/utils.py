1e05fe00cbb25f37989add06da0bd2cd4d6454ad
Sergei Gushin <ravenvl71@gmail.com>
2021-09-24 12:09:15 +0300

    add save and read param from db

--- utils/utils.py
+++ utils/utils.py
@@ -4,44 +4,91 @@ import dataset
 import csv
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
+from collections.abc import Iterable
+from requests_html import AsyncHTMLSession
+import asyncio
+import json
+
+
+def get_param_from_db(param_name):
+    db = dataset.connect('sqlite:///../data/icao_base.db')
+    table_settings = db['settings']
+    param = json.loads(table_settings.find_one(param=param_name)['content'])
+    db.close()
+    return param
+
+def set_param_to_db(param_name, content):
+    db = dataset.connect('sqlite:///../data/icao_base.db')
+    table_settings = db['settings']
+    add_dict = {
+        'param': param_name,
+        'content': json.dumps(content)
+    }
+    key = table_settings.find_one(param=param_name)
+    if key:
+        add_dict['id'] = key['id']
+        table_settings.update(add_dict, ['id'])
+    else:
+        table_settings.insert(add_dict)
+    db.close()
+
+async def get_name_port(icao_kod, asession, queue):
+    url = f'https://skyvector.com/airport/{icao_kod}'
+    r = await asession.get(url)
+    rez = r.html.find('div.titlebgrighta', first=True)
+    if rez:
+        print('rez=', rez.text)
+        await queue.put(icao_kod)
+
+
+async def check_sky_vector_icao_codes(icao_codes: Iterable, queue) -> list:
+    asession = AsyncHTMLSession()
+    tasks = []
+    for el in icao_codes:
+        task = asyncio.create_task(get_name_port(el, asession, queue))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
 
 def community_ikao():
     # DIR_COMMUNYTI = path.join('c:\\users\\raven\\appdata\\roaming\\microsoft flight simulator\\packages\\community\\')
-    DIR_COMMUNYTI = path.join('c:\\users\\raven\\appdata\\roaming\\microsoft flight simulator\\packages\\Official\\Steam\\')
+    DIR_COMMUNYTI = path.join(
+        'c:\\users\\raven\\appdata\\roaming\\microsoft flight simulator\\packages\\Official\\Steam\\')
     file_list = listdir(DIR_COMMUNYTI)
     db = dataset.connect('sqlite:///../data/icao_base.db')
     table_my = db['my_data']
-    temp = []
+    temp_port_check = set()
     temp_del = set()
     for name in file_list:
         reg_exp = r"-([a-zA-Z]{4})-"
         ikao_kod = re.findall(reg_exp, name)
         for test_name in ikao_kod:
-            port = db['ikao_data'].find_one(icao_code=test_name.upper())
-            if port:
-                port = table_my.find_one(icao_code=test_name.upper())
-                if not port:
-                    temp.append(name)
+            port_my_base = table_my.find_one(icao_code=test_name.upper())
+            if port_my_base:
+                break
+            port_icao_base = db['ikao_data'].find_one(icao_code=test_name.upper())
+            if port_icao_base:
+                temp_port_check.add(test_name)
             else:
-                port_comm = data_from_skyvector(test_name)
-                if port_comm.get('err', 0) == 0:
-                    port = table_my.find_one(icao_code=test_name.upper())
-                    if not port:
-                        temp.append(name)
+                temp_del.add(test_name)
 
-    print(temp)
+    queue = asyncio.Queue()
+    asyncio.run(check_sky_vector_icao_codes(temp_del, queue))
+    db.close()
+    print(queue)
 
 
 def convert_csv():
-
     db = dataset.connect('sqlite:///data/icao_base.db')
     with open('data/apinfo.ru.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter='|',)
+        spamreader = csv.reader(csvfile, delimiter='|', )
         for row in spamreader:
             with db as tx1:
                 tx1['icao_data'].insert(dict(icao_code=row[0], name_eng=row[2], city_eng=row[4], country_eng=row[6],
-                              iso_code=row[8], latitude=row[10], longitude=row[12], runway_length=row[14],
-                              runway_elevation=row[16]))
+                                             iso_code=row[8], latitude=row[10], longitude=row[12],
+                                             runway_length=row[14],
+                                             runway_elevation=row[16]))
 
 
 def data_from_opennav(icao_kod, rez):
@@ -67,11 +114,12 @@ def data_from_opennav(icao_kod, rez):
 
     return rez
 
+
 def data_from_skyvector(icao_kod):
     # OrderedDict([('id', 5676), ('icao_code', 'URMO'), ('name_eng', 'Beslan'), ('city_eng', 'Vladikavkaz'),
     #              ('country_eng', 'Russian Federation'), ('iso_code', 'RU'), ('latitude', '43.205114'),
     #              ('longitude', '44.606642'), ('runway_length', '3000'), ('runway_elevation', '510')])
-    rez ={'icao_code': icao_kod}
+    rez = {'icao_code': icao_kod}
     url = f'https://skyvector.com/airport/{icao_kod}'
     try:
         response = requests.get(url)
@@ -98,25 +146,29 @@ def data_from_skyvector(icao_kod):
     for el in arp_data:
         try:
             if el.find(string='Dimensions:'):
-                rez['runway_length'] = float(el.find('td').text.split()[0])/3.281
+                rez['runway_length'] = float(el.find('td').text.split()[0]) / 3.281
         except IndexError as e:
             rez['runway_length'] = 0
         try:
             if el.find(string='Elevation:'):
-                rez['runway_elevation'] = float(el.find('td').text)/3.281
+                rez['runway_elevation'] = float(el.find('td').text) / 3.281
         except IndexError as e:
             rez['runway_elevation'] = 0
     rez = data_from_opennav(icao_kod, rez)
     return rez
 
-    # receive country code from opennav
-
-
-
 
 if __name__ == '__main__':
     # community_ikao()
-    # print(data_from_skyvector('URMO'))
-    community_ikao()
-
-
+    # print(data_from_skyvector('URM1'))
+    # community_ikao()
+    # from requests_html import HTMLSession
+    # session = HTMLSession()
+    # url = f'https://skyvector.com/airport/URM1'
+    # r = session.get(url)
+    # rez = r.html.find('div.titlebgrighta', first=True)
+    # arr = ['c:\\users\\raven\\appdata\\roaming\\microsoft flight simulator\\packages\\Official\\Steam\\',
+    #        'c:\\users\\raven\\appdata\\roaming\\microsoft flight simulator\\packages\\community\\']
+    # set_param_to_db('path_to_community', arr)
+    rez = get_param_from_db('path_to_community')
+    print(rez)
